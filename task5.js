39bbe8fe245a181666ca05e2f913632b04dcc552
0rangeGuy <148427436+0rangeGuy@users.noreply.github.com>
2023-12-23 09:56:51 +0700

    Add files via upload

---
+++ task5.js
@@ -0,0 +1,7 @@
+alert('Введите 2 числа');
+let DigitOne = prompt('Первое число: ');
+let DigitTwo = prompt('Второе число: ');
+DigitOne = Number(DigitOne);
+DigitTwo = Number(DigitTwo);
+let Result = DifitOne%DigitTwo;
+alert('Результат: '+ Result);
